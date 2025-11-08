6e91fe898bd3e7d9da7593ca2a585ca52df9e516
Matheus P Silva <matheusptws@gmail.com>
2025-11-08 11:33:07 -0300

    feat: add text verificador

---
+++ verificador.py
@@ -0,0 +1,8 @@
+nome = input("Qual é o seu nome? ")
+idade = int(input("Qual é a sua idade? "))
+
+if idade >= 18:
+    print(f"Olá {nome}, você é de maior")
+
+else:
+    print(f"Olá {nome}, você é de menor")
