455e2032eb94c08831c84173293d94e473a3ec51
Matheus P Silva <matheusptws@gmail.com>
2025-11-11 21:23:06 -0300

    feat: add ativiade_3

---
+++ funcao_calculadora.py
@@ -0,0 +1,25 @@
+def calculadora(numero1, numero2, operacao):
+    if operacao == '+':
+        return numero1 + numero2
+
+    if operacao == '-':
+        return numero1 - numero2
+
+    if operacao == '*':
+        return numero1 * numero2
+
+    if operacao == '/':
+        if numero2 != 0:
+            return numero1 / numero2
+        else:
+            return "Erro: divisão por zero"
+
+    return "Operação inválida"
+
+# ex
+
+numero1 = float(input("Digite o primeiro número: "))
+operacao = input("Digite a operação (+, -, *, /): ")
+numero2 = float(input("Digite o segundo número: "))
+resultado = calculadora(numero1, numero2, operacao)
+print("Resultado:", resultado)
